84f98f8f29592e06dfc7c92b60be7140409fc140
mojtaba <mojtabaeuler@gmail.com>
2022-07-29 15:25:00 +0430

    Add Flask simple app to the project

--- ComponentBuilder/Components/WebApplication/flask/component.py
+++ ComponentBuilder/Components/WebApplication/flask/component.py
@@ -1,6 +1,65 @@
+import os
+
+from jinja2 import Template
+import glob
+
+
 from ComponentBuilder.Components.abstract import Component
+from Program.nosy import Nosy
 
 
 class Flask(Component):
+    def __init__(self, component):
+        super().__init__(component)
+        self.file_mapper = {}
+        for jinja_file in glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__))) + '/*.jinja'):
+            with open(jinja_file, "r") as f:
+                self.file_mapper[jinja_file.split("/")[-1]] = f.read()
+
+    @property
+    def template_arguments(self):
+        return {
+            "internal_port": self.component["specs"]["internal-port"],
+            "main_py_route": self.component["specs"]["config"]["locations"][0].split(":")[-1],
+            "python_docker_image_with_version": self.get_python_docker_image_with_version(),
+            "flask_version": "2.1.3",
+            "docker_compose": self.generate_docker_compose_variables(),
+        }
+
+    def generate_docker_compose_variables(self):
+        project_name = Nosy.ask_project_name()
+        docker_compose_variables = {
+            "version": self.docker_compose_version,
+            "service_name": self.component["specs"]["name"],
+            "build_dir": "{}/{}".format(project_name, self.component["name"]),
+            "network_name": "{}-net".format(project_name),
+        }
+
+        return docker_compose_variables
+
+    def get_python_docker_image_with_version(self):
+        python_version = "3.9"
+        if self.component["specs"]["config"].get("python-version"):
+            python_version = self.component["specs"]["config"]["python-version"]
+        return "python:{}".format(python_version)
+
     def run(self):
-        return "Django"
+        self.render("app.py")
+        self.render("requirements.txt")
+        self.render("Dockerfile")
+        self.render("docker-compose.yml")
+
+        self.go_back_to_project_directory()
+
+    def render(self, file_name):
+        with open(self.abs_location + "/" + file_name, "w") as f:
+            rendered_content = Template(
+                self.file_mapper["{}.jinja".format(file_name)],
+                trim_blocks=True,
+                lstrip_blocks=True,
+            ).render(**self.template_arguments)
+            f.write(rendered_content)
+
+    @staticmethod
+    def go_back_to_project_directory():
+        os.chdir("..")
--- tests/test_flask_builder.py
+++ tests/test_flask_builder.py
@@ -3,7 +3,7 @@ import shutil
 import unittest
 import io
 
-from ComponentBuilder.Components.WebServer import nginx
+from ComponentBuilder.Components.WebApplication import flask
 from ComponentBuilder.main import ComponentBuilder
 
 from Program.ConfigLoaders import YAMLConfigLoader
@@ -13,8 +13,10 @@ from tests.specs import get_list_of_components
 
 class NginxServiceBuilderTestCase(unittest.TestCase):
     def setUp(self) -> None:
+        self.config_file_location = "tests/configs/flask_config.yml"
         self.test_folder_location = os.getcwd()
-        self.nginx_config_dictionary = get_list_of_components("tests/configs/nginx_config.yml")
+        print(self.test_folder_location)
+        self.nginx_config_dictionary = get_list_of_components(self.config_file_location)
         self.component_builder = ComponentBuilder(self.nginx_config_dictionary["components"][0])
         self.project_location = self.test_folder_location + "/" + self.nginx_config_dictionary["project"]["name"]
         self.component_name = self.nginx_config_dictionary["components"][0]["specs"]["name"]
@@ -22,7 +24,7 @@ class NginxServiceBuilderTestCase(unittest.TestCase):
             os.mkdir(self.project_location)
 
     def test_is_folder_and_files_created_properly_after_run(self):
-        Nosy.load(YAMLConfigLoader("tests/configs/general_config.yml").load_file())
+        Nosy.load(YAMLConfigLoader(self.config_file_location).load_file())
         self.component_builder.run()
         os.chdir(self.project_location)
         component_directory = self.component_builder.component["specs"]["name"]
@@ -30,17 +32,21 @@ class NginxServiceBuilderTestCase(unittest.TestCase):
         self.assertEqual(self.component_builder.component["location"], "{}/{}".format(self.project_location, self.component_name))
         self.assertEqual(self.component_builder.component_instance.abs_location, "{}/{}".format(self.project_location, component_directory))
 
-        self.assertTrue(os.path.isfile(component_directory + "/nginx.conf"), msg="File nginx.conf not created")
-        self.assertTwoFileEqual(component_directory + "/nginx.conf",
-                                os.path.dirname(nginx.__file__) + "/examples/config.nginx")
+        self.assertTrue(os.path.isfile(component_directory + "/app.py"), msg="File app.py not created")
+        self.assertTwoFileEqual(component_directory + "/app.py",
+                                os.path.dirname(flask.__file__) + "/examples/app.py")
 
         self.assertTrue(os.path.isfile(component_directory + "/Dockerfile"), msg="File Dockerfile not created")
         self.assertTwoFileEqual(component_directory + "/Dockerfile",
-                                os.path.dirname(nginx.__file__) + "/examples/Dockerfile")
+                                os.path.dirname(flask.__file__) + "/examples/Dockerfile")
+
+        self.assertTrue(os.path.isfile(component_directory + "/requirements.txt"), msg="File requirements.txt not created")
+        self.assertTwoFileEqual(component_directory + "/requirements.txt",
+                                os.path.dirname(flask.__file__) + "/examples/requirements.txt")
 
         self.assertTrue(os.path.isfile(component_directory + "/docker-compose.yml"), msg="File docker-compose.yml not created")
         self.assertTwoFileEqual(component_directory + "/docker-compose.yml",
-                                os.path.dirname(nginx.__file__) + "/examples/docker-compose.yml")
+                                os.path.dirname(flask.__file__) + "/examples/docker-compose.yml")
 
     def assertTwoFileEqual(self, file_1, file_2):
         with io.open(file_1) as f1, io.open(file_2) as f2:
